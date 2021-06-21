ac67d237520659222294b44597133040b85a4a24
Genius HKR <geniushkr81Qgmail.com>
2021-06-21 10:56:01 -0500

    update 001

--- src/members/schemas/member.schema.ts
+++ src/members/schemas/member.schema.ts
@@ -3,6 +3,7 @@ import * as mongoose from 'mongoose';
 const AutoIncrement = require('mongoose-sequence')(mongoose);
 
 export const MemberSchema = new mongoose.Schema({
+    _id: Number,
     name:String,
     address: String,
     reference1:String,
@@ -13,8 +14,8 @@ export const MemberSchema = new mongoose.Schema({
     talents:Number,
     occupation:String,
 },
-{   
+{   _id:false,
     versionKey:false
 });
 
-MemberSchema.plugin( AutoIncrement, {inc_field: 'id'});
+MemberSchema.plugin( AutoIncrement);
